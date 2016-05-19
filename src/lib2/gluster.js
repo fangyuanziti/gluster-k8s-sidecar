628e0cd9e37159d81cecbeeb0a2c925d5831c15d
neshte <elbuenjim@gmail.com>
2016-05-19 18:38:49 -0500

    bad logic in loop for replication multiples detection of nodes

--- src/lib2/gluster.js
+++ src/lib2/gluster.js
@@ -244,7 +244,7 @@ var getOrphanPodsInMultiplesOf = function(ctx, done){
                     }
                 }
                 if(completeset === true){
-                    for(var j=0; j<ctx.glusterpods.length; j+=1){
+                    for(var j=0; j<ctx.replication; j+=1){
                         var pod = ctx.glusterpods[i+j];
                         tasks.push(function(cb){
                             var thispod = pod;
